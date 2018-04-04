43b26e5d2a6f1bb0836fe6cc2afcde5df88bdd7c
ZapAnton <wc3ft2@gmail.com>
2018-04-04 17:10:33 +0300

    Reading data file line by line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
 fn main() {
-    println!("Hello, world!");
+    let data_file = File::open("data/data1.txt").expect("Error opening data file!");
+
+    for line in BufReader::new(data_file).lines() {
+        println!("{}", line.unwrap());
+    }
 }
