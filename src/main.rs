b5501d4677c609e922724d208b303f304e2cb997
ZapAnton <wc3ft2@gmail.com>
2018-04-04 18:50:37 +0300

    Moved argument parsing, file parsing and finding most frequent word to separate functions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 use std::{env, collections::HashMap, fs::File, io::{BufRead, BufReader}};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        panic!("You need to provide path to data file to parse!");
-    }
-
-    let file_path = &args[1];
-
-    let data_file = File::open(file_path).expect("Error opening data file!");
-
+fn read_words_from_file(data_file: &File) -> HashMap<String, i32> {
     let mut words: HashMap<String, i32> = HashMap::new();
 
     for line in BufReader::new(data_file).lines() {
@@ -31,16 +21,38 @@ fn main() {
         }
     }
 
-    let mut max_entry = ("", 0);
+    words
+}
 
-    for (key, value) in &words {
+fn find_most_frequent_word(words: &HashMap<String, i32>) -> (String, i32) {
+    let mut max_entry = (String::new(), 0);
+
+    for (key, value) in words {
         if value > &max_entry.1 {
-            max_entry = (key, *value);
+            max_entry = (key.to_string(), *value);
         }
     }
 
+    max_entry
+}
+
+fn get_parse_file_name() -> String {
+    env::args()
+        .nth(1)
+        .expect("You need to provide path to data file to parse!")
+}
+
+fn main() {
+    let file_path = get_parse_file_name();
+
+    let data_file = File::open(file_path).expect("Error opening data file!");
+
+    let words = read_words_from_file(&data_file);
+
+    let max_entry = find_most_frequent_word(&words);
+
     println!(
-        "The most frequent word is {}. The data file contains it {} times",
+        "The most frequent word is '{}'. The data file contains it {} times",
         max_entry.0, max_entry.1
     );
 }
